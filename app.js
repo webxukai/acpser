b297909f34e32e3a89beffbac6750769ab28e21f
xukai <kai.xu@datacvg.com>
2019-07-03 16:27:19 +0800

    远程连接msq,redis

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
+
+const cookieParase = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +12,17 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//这点很重要，如果没有，下面的req.cookies 会返回undefined
+app.use(cookieParase());
+
+app.use(cookieSession({
+  //session的秘钥，防止session劫持。 这个秘钥会被循环使用，秘钥越长，数量越多，破解难度越高。
+  keys: ['aaa', 'bbb', 'ccc'],
+  //session过期时间，不易太长。php默认20分钟
+  maxAge: 60 * 60,
+  //可以改变浏览器cookie的名字
+  name: 'session'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +30,9 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +40,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,92 @@
 var express = require('express');
+
 var router = express.Router();
+
+var redis = require('redis')
+
+// var client = redis.createClient(6379, '127.0.0.1')
+// client.on('error', function (err) {
+//   console.log('Error ' + err);
+// });
+var client = redis.createClient(6379, '129.211.47.103')
+client.on('error', function (err) {
+  console.log('Error ' + err);
+});
+
+// 1 键值对
+client.set('color', 'red', redis.print);
+client.get('color', function (err, value) {
+  if (err) throw err;
+  console.log('Got: ' + value)
+  client.quit();
+})
+
+client.hmset('kitty', {
+  'age': '2-year-old',
+  'sex': 'male'
+}, redis.print);
+client.hget('kitty', 'age', function (err, value) {
+  if (err) throw err;
+  console.log('kitty is ' + value);
+});
+
+//哈希表
+client.hkeys('kitty', function (err, keys) {
+  if (err) throw err;
+  keys.forEach(function (key, i) {
+    console.log(key, i);
+  });
+  client.quit();
+});
+
+//链表
+client.lpush('tasks', 'Paint the house red.', redis.print);
+client.lpush('tasks', 'Paint the house green.', redis.print);
+client.lrange('tasks', 0, -1, function (err, items) {
+  if (err) throw err;
+  items.forEach(function (item, i) {
+    console.log(' ' + item);
+  });
+  client.quit();
+});
+
+// 集合
+client.sadd('ip', '192.168.3.7', redis.print);
+client.sadd('ip', '192.168.3.7', redis.print);
+client.sadd('ip', '192.168.3.9', redis.print);
+client.smembers('ip', function (err, members) {
+  if (err) throw err;
+  console.log(members);
+  client.quit();
+});
+
+// // 信道
+// var redis = require('redis')
+// var clientA = redis.createClient(6379, '127.0.0.1')
+// var clientB = redis.createClient(6379, '127.0.0.1')
+
+// clientA.on('message', function (channel, message) {
+//   console.log('Client A got message from channel %s: %s', channel, message);
+// });
+// clientA.on('subscribe', function (channel, count) {
+//   clientB.publish('main_chat_room', 'Hello world!');
+// });
+// clientA.subscribe('main_chat_room');
 
 //使用mysql中间件连接MySQL数据库
 const mysql = require('mysql')
+// const connection = mysql.createConnection({
+//   host: 'localhost', //数据库地址
+//   user: 'root', //用户名
+//   password: 'newpassword', //密码
+//   port: '3306', //端口
+//   database: 'acpser', //库名
+//   multipleStatements: true //允许执行多条语句
+// })
 const connection = mysql.createConnection({
-  host: 'localhost', //数据库地址
+  host: '129.211.47.103', //数据库地址
   user: 'root', //用户名
-  password: 'newpassword', //密码
+  password: '123456', //密码
   port: '3306', //端口
   database: 'acpser', //库名
   multipleStatements: true //允许执行多条语句
@@ -35,9 +115,12 @@ router.post('/login', function (req, res) {
   let agentPwd = req.body.agentPwd
   console.log(account)
   console.log(agentPwd)
+  res.cookie("name",'zhangsan',{maxAge: 900000, httpOnly: true});  
+  // res.cookie('user', 'lililiwen');
+  console.log(req.cookies);
 
-  // const sql = 'SELECT * FROM user_info' //user_info 为表名
-  const sql_select = `SELECT *  FROM user_info WHERE user_name = '${account}' AND user_password = '${agentPwd}'` //user_info 为表名
+  const sql = 'SELECT * FROM userInfo' //userInfo 为表名
+  const sql_select = `SELECT *  FROM userInfo WHERE userName = '${account}' AND userPassword = '${agentPwd}'` //user_info 为表名
 
   connection.query(sql_select, (err, results) => {
     console.log(111)
